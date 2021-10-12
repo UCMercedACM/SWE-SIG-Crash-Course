beb4cb7020c9cbd466a9430e299507f697344a0b
Jatnael Montes <ifelsejet@gmail.com>
2021-10-12 10:56:34 -0700

    Added more content

---
+++ w4-Intro-APIs/index.js
@@ -0,0 +1,10 @@
+fetch('https://jsonplaceholder.typicode.com/posts').then(function (response) { //try going to that link!
+	// The API call was successful!
+	return response.json();
+}).then(function (data) {
+	// This is the JSON from our response
+	console.log(data);
+}).catch(function (err) {
+	// There was an error
+	console.warn('Something went wrong.', err);
+});
